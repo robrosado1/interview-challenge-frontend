ee96ea12da2a7e63b5f6001b3504a8794d9d987d
Robinson <robrosado1@gmail.com>
2024-07-18 13:09:36 -0700

    *shakes fist at Google Meet*

--- src/components/PokemonList.jsx
+++ src/components/PokemonList.jsx
@@ -4,9 +4,9 @@ import PaginationButton from './PaginationButton';
 const PAGE_LIMIT = 20;
 
 const PokemonList = () => {
-  const pokemonList = []; // TODO: Use the `pokemonList` state
-  const currentPage = 1; // TODO: Use the `currentPage` state
-  const totalPages = 0; // TODO: Use the `totalPages` state
+  const [pokemonList, setPokemonList] = React.useState([]);
+  const [currentPage, setCurrentPage] = React.useState(0);
+  const [totalPages, setTotalPages] = React.useState(0);
 
   const fetchPokemonList = async (page) => {
     // TODO: Fetch the list of pokemons based on the current page
@@ -15,16 +15,24 @@ const PokemonList = () => {
     // Update the `totalPages` state based on the total number of pokemons
     // Update the `pokemonList` state with the fetched pokemons
     // Handle loading and error states
-    const offset = 0; // TODO: Calculate the offset based on the current page
-    const response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${PAGE_LIMIT}&offset=`);
+    const offset = currentPage * PAGE_LIMIT; // TODO: Calculate the offset based on the current page
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${PAGE_LIMIT}&offset=${offset}`);
     const data = await response.json();
+    console.log(data);
+    setPokemonList(data.results);
+    setTotalPages(Math.floor(data.count / PAGE_LIMIT));
   };
+
+  React.useEffect(() => {
+    fetchPokemonList();
+  }, [currentPage]);
 
   // TODO: Add loading and error states
 
   return (
     <div className="container mx-auto p-4">
       <div className="flex flex-wrap -mx-2">
+        <ul>
         {pokemonList.map(pokemon => {
           const pokemonId = pokemon.url; // TODO: Extract the pokemon ID from the URL
           return (
@@ -33,21 +41,40 @@ const PokemonList = () => {
             // Use the official artwork URL for the pokemon image
             // Use the pokemon name as the link text
             // Add basic styling for the pokemon card using tailwindcss
-            <div></div>
+            <li>
+              <div>
+                <span>Name: {pokemon.name}</span>
+                {" "}
+                <img src='' />
+              </div>
+            </li>
           );
         })}
+        </ul>
       </div>
       <div className="flex justify-center mt-4">
         <PaginationButton
           type="Previous"
-          page={currentPage}
-          setPage={() => {}} // TODO: Implement the setPage function
+          page={currentPage + 1}
+          setPage={() => {
+            if (currentPage > 0) {
+              setCurrentPage((prev) => prev - 1);
+            } else {
+              console.error("Already at beginning of list");
+            }
+          }} // TODO: Implement the setPage function
         />
-        <span className="px-4 py-2 mx-1">{currentPage} / {totalPages}</span>
+        <span className="px-4 py-2 mx-1">{currentPage + 1} / {totalPages}</span>
         <PaginationButton
           type="Next"
           page={currentPage}
-          setPage={() => {}} // TODO: Implement the setPage function
+          setPage={() => {
+            if (currentPage < totalPages) {
+              setCurrentPage((prev) => prev + 1);
+            } else {
+              console.error("Already at end of list");
+            }
+          }} // TODO: Implement the setPage function
         />
       </div>
     </div>
